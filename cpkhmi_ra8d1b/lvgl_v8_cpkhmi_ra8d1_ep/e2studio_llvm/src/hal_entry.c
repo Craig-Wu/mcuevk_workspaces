9b404fb7a0c683d89e5f378d2dea475188e7dbea
GJ Huang <gj.huang.uj@renesas.com>
2024-07-04 15:03:35 +0800

    update license of some files

--- cpkhmi_ra8d1b/lvgl_v8_cpkhmi_ra8d1_ep/e2studio_llvm/src/hal_entry.c
+++ cpkhmi_ra8d1b/lvgl_v8_cpkhmi_ra8d1_ep/e2studio_llvm/src/hal_entry.c
@@ -1,3 +1,8 @@
+/*
+* Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
 #include "hal_data.h"
 // #include "dsi_layer.h"
 #include "lvgl.h"
